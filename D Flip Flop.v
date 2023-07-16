3f7fe7eaf709e8b3d4e972224b1c0081458373dc
Ashish Patidar <ashishpatidar1009993@gmail.com>
2023-07-16 19:49:04 +0530

    Create D Flip Flop.v

---
+++ D Flip Flop.v
@@ -0,0 +1,11 @@
+module D_FF (q, d, rst_n, clk,init_value);  
+ output q; 
+ input d, rst_n, clk,init_value;  
+ reg q; // Indicate that q is stateholding  
+ always @(posedge clk or negedge rst_n)  
+ if (~rst_n)  
+ q <= init_value;     // On reset, set to 0  
+ else  
+ q <= d; // Otherwise out = d   
+
+ endmodule
